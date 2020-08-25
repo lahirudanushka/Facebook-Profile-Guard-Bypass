20e28d8937ef100bf49dd7c7f431d6e0b7919bfe
lahirudanushka <hirud94@gmail.com>
2020-08-25 23:08:51 +0530

    Project updated

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function getImage(){
 	var uid = document.getElementById('uid').value;
-	if(uid!=""){
-	//console.log(uid);
-
-	var link =  "https://graph.facebook.com/"+uid+"/picture?width=8000";
+	letterNumber = /^[0-9]+$/;
+	if((uid.value.match(letterNumber)){
+		var link =  "https://graph.facebook.com/"+uid+"/picture?width=8000";
 
 	window.open(link, '_blank'); win.focus();
-}
+
+			}
+	
+	
+
+	
 }
